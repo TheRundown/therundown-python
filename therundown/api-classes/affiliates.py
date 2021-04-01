c5910a00eae237741fb21a9cdf002f353c91587b
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-03-31 17:26:03 -0700

    Add build and test workflow

--- therundown/api-classes/affiliates.py
+++
@@ -1,9 +0,0 @@
-class Affiliates:
-    def __init__(**kwargs):
-        """GET /affiliates
-
-        Returns:
-            list of resources.Affiliate
-        """
-        super().__init__(**kwargs)
-        # build URL and make call to self._get()
