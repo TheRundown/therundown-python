0adc97ea8186c2e2ebf037e8961e60694a300909
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-04 00:19:53 -0700

    Add UTC shift function

--- rundown/utils.py
+++ rundown/utils.py
@@ -12,3 +12,12 @@ def utc_offset(timezone: str) -> int:
     """
     t_delta = arrow.now(timezone).utcoffset()
     return int(t_delta.days * 24 * 60 + t_delta.seconds / 60)
+
+
+def utc_shift(timezone: str) -> int:
+    """Amount to shift UTC time in minutes for timezone.
+
+    Example:
+        Phoenix is 7 hours behind UTC, so midnight in Phoenix is UTC midnight + 7 hours.
+    """
+    return -utc_offset(timezone)
