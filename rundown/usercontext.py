3eb4627a897d53af6bdc9523fc79a13da22e4283
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-06 12:53:11 -0700

    Simplify module, remove unnecessary functions

--- rundown/usercontext.py
+++ rundown/usercontext.py
@@ -4,18 +4,13 @@ from contextlib import contextmanager
 context_timezone = ContextVar("context_timezone")
 
 
-def set_context_var(var, val):
-    if val is not None:
-        return var.set(val)
-
-
-def reset_context_var(var, val, token):
-    if val is not None:
-        return var.reset(token)
-
-
 @contextmanager
-def user_context(timezone=None, odds_type=None):
-    token_timezone = set_context_var(context_timezone, timezone)
+def user_context(timezone: str):
+    """Context manager used by Rundown in order to pass state to Pydantic validators.
+
+    Args:
+        timezone: A timezone string.
+    """
+    token_timezone = context_timezone.set(timezone)
     yield
-    reset_context_var(context_timezone, timezone, token_timezone)
+    context_timezone.reset(token_timezone)
