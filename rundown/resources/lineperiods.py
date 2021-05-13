c83dda7ec6a18e351b9358db3de243ffa43052c8
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-05-13 16:09:07 -0700

    Add default empty list on optional attributes

--- rundown/resources/lineperiods.py
+++ rundown/resources/lineperiods.py
@@ -7,10 +7,10 @@ from rundown.resources.line import MoneylinePeriod, SpreadPeriod, TotalPeriod
 
 class LinePeriods(BaseModel):
     period_full_game: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
-    period_first_half: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
-    period_second_half: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
-    period_first_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
-    period_second_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
-    period_third_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
-    period_fourth_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
-    period_live_full_game: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]]
+    period_first_half: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]] = []
+    period_second_half: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]] = []
+    period_first_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]] = []
+    period_second_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]] = []
+    period_third_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]] = []
+    period_fourth_period: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]] = []
+    period_live_full_game: list[Union[MoneylinePeriod, SpreadPeriod, TotalPeriod]] = []
